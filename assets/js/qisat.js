cd7d121ee34e222b5a0dbb34647848fd493ebcd0
Leonardo Della Giustina <leonardo.giustina@MNTEC.COM.BR>
2016-07-25 19:04:14 -0300

    Comunicação com API - buscar lista de instrutores e monta lista no HOME

--- assets/js/qisat.js
+++ assets/js/qisat.js
@@ -1,5 +1,38 @@
-angular.module("appQiSat", []);	
+var appQiSat = angular.module("appQiSat", []);	
 
-angular.module("appQiSat").controller("appCtl", function($scope){
-	$scope.msg = "teste" ;
-});	
+appQiSat.value("config", {
+	baseUrl : "http://webservice.qisat.com:3000",
+	imagensUrl : "http://webservice.qisat.com:3000/imagens/instrutores/",
+	imagensUrlDefault : "http://webservice.qisat.com:3000/imagens/instrutores/instrutor.png"
+});
+
+appQiSat.controller("appCtrlHome", 
+			function($scope, QiSatAPI, config){
+						$scope.instructors = [];
+						QiSatAPI.getInstructorsTop()
+								.then( function ( response ){
+									var instructors = [];
+									if(response.status == 200) instructors = response.data;
+									instructors.map( function (instructor) {
+										if(instructor)
+											if(!instructor.imagem)
+												instructor.imagem = config.imagensUrlDefault;
+											else
+												instructor.imagem = config.imagensUrl+instructor.imagem;
+									});
+									$scope.instructors = instructors;
+								 });
+			});
+
+
+appQiSat.factory("QiSatAPI", function($http, config){
+
+	var _getInstructorsTop = function () {
+		return $http({ method: 'GET', url: config.baseUrl+'/instrutores/top'});
+	};
+
+	return {
+		getInstructorsTop : _getInstructorsTop
+	};
+
+});
