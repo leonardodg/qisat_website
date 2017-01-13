1e13a8b310fb9ab1e4d9e88bee960fa65f643df6
LeoDG <leonardodg@msn.com>
2017-01-13 09:29:11 -0300

    Controller Access Router in Run

--- assets/js/services/authService.js
+++ assets/js/services/authService.js
@@ -306,24 +306,6 @@
 			    					certificados : certificados
 								};
 					};
-				})
-				.run(['$rootScope', '$location', '$route' ,'authService', 
-				    function ($rootScope, $location, $route, authService ) {
-
-						$rootScope.$on('$routeChangeStart', function(e, curr, prev) {
-							$rootScope.error = '';
-							if (curr && curr.$$route && !curr.$$route.isAuth && authService.isLogged() && ($location.path().indexOf('lembrete-de-senha') < 0)) {
-								window.location = '/aluno/perfil';
-							}
-						});
-
-						$rootScope.$on('$routeChangeSuccess', function(e, curr, prev) {
-							if (curr && curr.$$route && curr.$$route.isAuth && !authService.isAuth() ){
-								$rootScope.error = "Acesso Restrito!";
-								window.location = '/login';
-							}
-						});
-
-			    }]);
+				});
 
 })();
