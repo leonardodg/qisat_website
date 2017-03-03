ab0bf4689b91a2921682c0e06c658a04bc72043e
Deyvison-Fernandes <deyvison_fernandes@hotmail.com>
2017-03-03 09:16:08 -0300

    Atualização Designer QiSat - Textos

--- assets/js/config/value.js
+++ assets/js/config/value.js
@@ -6,10 +6,10 @@
 		.value("Config", {
 				
 				Authorization : "Digest",
-       			baseUrl : "http://local-ecommerce.qisat.com.br:83/",
+       			baseUrl : "http://ecommercelocal.qisat.com.br",
 				imagensUrlDefault : "/images/instructors/instrutor.png",
 				imgCursoUrlDefault : "/images/courses/curso-online.jpg",
-				boletoUrl : "http://local-ecommerce.qisat.com.br:83/forma-pagamento-boleto-php/boleto",
+				boletoUrl : "http://ecommercelocal.qisat.com.br/forma-pagamento-boleto-php/boleto",
 
 				convenio : { "descontoAluno" : 30, "descontoAssociado" : 20, "descontoProfessor" : 50, "descontoCREAS" : 30},
 
