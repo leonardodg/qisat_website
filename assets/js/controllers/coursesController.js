601c9fd5d7fe13133e1fa41051e9573ad15d3b19
Inty Castillo <inty.castillo@MNTEC.COM.BR>
2017-08-21 10:11:39 -0300

    Correção do filtro de pesquisa
    Alteração do carregamento e do filtro por caracter especial e letras maiusculas e minusculas.

--- assets/js/controllers/coursesController.js
+++ assets/js/controllers/coursesController.js
@@ -139,7 +139,7 @@
 
 
 								vm.coursesList = QiSatAPI.getCourseList();
-								
+
 								switch($location.path()){
 										case '/cursos/online/serie':
 											setFilterTipo('Combinacoes', 32, '#series');
@@ -243,9 +243,22 @@
 						};
 
 						vm.myFilter = function (item) {
+							function retira_acentos(palavra) {
+								var com_acento = 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ';
+								var sem_acento = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
+								var nova = '';
+								for(var i=0;i<palavra.length;i++) {
+									if (com_acento.search(palavra.substr(i,1))>=0) {
+										nova += sem_acento.substr(com_acento.search(palavra.substr(i,1)),1);
+									} else {
+										nova += palavra.substr(i,1);
+									}
+								}
+								return nova;
+							};
 							if(vm.search)
-								return ((item.nome && item.nome.indexOf(vm.search) >= 0) 
-										|| (item.info && item.info['metatag_key'].indexOf(vm.search)>= 0));
+								return ((item.nome && retira_acentos(item.nome).toLowerCase().indexOf(retira_acentos(vm.search).toLowerCase()) >= 0)
+									 || (item.info && retira_acentos(item.info['metatag_key']).toLowerCase().indexOf(retira_acentos(vm.search).toLowerCase())>= 0));
 							return true;
 						};
 
@@ -264,7 +277,7 @@
 									});
 									filter.map(function(el){ el.show = false; el.selected = false; });
 
-									// REGRAS DE COMPORTAMENTE ELEMENTOS DA PÁGINA 
+									// REGRAS DE COMPORTAMENTE ELEMENTOS DA PÁGINA
 									elemts = angular.element('.filter-types-produts:checked, .filter-area:checked, #Tipo0, #Tipo1');
 									elemts.map( function (el) { elemts[el].checked = false });
 
@@ -299,15 +312,15 @@
 									 	selected = angular.element('#Tipo1')[0];
 									}
 
-  									filter = vm.coursesList.filter( function (el){ 
+  									filter = vm.coursesList.filter( function (el){
 										return (((el.type == 2 || el.type == 10 || el.type == 32 || el.type == 17 || el.type == 'eventos' ) && el.show ) || ((el.parent == 2 || el.parent == 32 || el.parent == 17  || el.parent == 10 || el.parent == 'eventos' ) && el.selected ))
 									});
 
-									filter.map(function (list){ 
+									filter.map(function (list){
 										var courses, type, outherType;
 
 										if(item.presencial && (list.type == 10 || list.type == 12 || list.type == 'eventos')){
-											type = item.presencial;	
+											type = item.presencial;
 											outherType = item.presencial;
 										}else{
 											type = item.type;
@@ -347,21 +360,21 @@
 									 	selected = angular.element('#Tipo1')[0];
 									}
 
-									filter = vm.coursesList.filter( function (el){ 
+									filter = vm.coursesList.filter( function (el){
 											return(((el.type == 2 || el.type == 10 || el.type == 32 || el.type == 17 || el.type == 'eventos' ) && el.show )||((el.parent == 2 || el.parent == 32 || el.parent == 17  || el.parent == 10 || el.parent == 'eventos' ) && el.selected ))
 										});
 
 									if(!selected.checked){
-										filter.map(function (list){ 
+										filter.map(function (list){
 											list.courses.map( function (course){ course.show = true });
 										 });
 									}else{
 
-										filter.map(function (list){ 
+										filter.map(function (list){
 											var coursesUnselected, coursesSelected, type, outherType;
 
 											if(item.presencial && (list.type == 10 || list.type == 12 || list.type == 'eventos')){
-												type = item.presencial;	
+												type = item.presencial;
 												outherType = (item.presencial == 11) ? 13 : 11;
 											}else{
 												type = item.type;
@@ -387,7 +400,7 @@
 						};
 
 						vm.filterStates = function ( item ) {
-							
+
 								var coursesList, filter, selected, elemts;
 									selected = vm.coursesList.find(function (el) { return el.type == 'eventos' });
 
@@ -443,7 +456,7 @@
 									elemts.map(function (el){ if (el && (elemts[el]) && elemts[el].selected ) angular.element(elemts[selected]).prop('selected', false); });
 
 
-									filter = vm.coursesList.filter( function (el){ return ( el.parent == 10 || el.parent == 12 || el.parent == 'eventos' ) && el.selected });	
+									filter = vm.coursesList.filter( function (el){ return ( el.parent == 10 || el.parent == 12 || el.parent == 'eventos' ) && el.selected });
 									if(!filter.length)
 										filter = vm.coursesList.filter( function (el){ return ( el.type == 10 || el.type == 12 || el.type == 'eventos' ) && !el.show });
 									filter.map(function(el){ if(el.courses.length) el.show = true });
@@ -461,7 +474,7 @@
 							var item = inputStates.inputs.find(function (el) {return el.id == id });
 							var elemts, selected = null;
 							if(item){
-									
+
 								elemts = angular.element('#selectStates option');
 								elemts.map(function (el){ if (el && (elemts[el]) && elemts[el].value == item.uf ) selected = el; });
 
@@ -488,7 +501,7 @@
 								if(!filter.length) selected.show = true;
 								else filter.map(function (el){ if(el.courses.length && !el.show ) el.show = true });
 
-								filter = vm.coursesList.filter( function (el){ 
+								filter = vm.coursesList.filter( function (el){
 									return ((el.type == 2 || el.type == 10 || el.parent == 2 || el.parent == 10 || el.type == 'eventos' || el.parent == 'eventos' || el.type == 12 || el.parent == 12 ) && el.show );
 								});
 								filter.map(function(el){ el.show = false });
@@ -507,7 +520,7 @@
 								if(selected){
 									angular.element(elemts[selected]).prop('selected', false);
 									angular.element(elemts[0]).prop('selected', true);
-									// elemts[selected].selected = false; 
+									// elemts[selected].selected = false;
 									// elemts[0].selected = true;
 									resetFilterEvents();
 								}
@@ -551,7 +564,7 @@
 										navLink.selected = true;
 									else
 										navParent.inputs.push({selected : true, name: name, hash : '#'+name, type: item.type });
-									
+
 									navParent.selected++;
 								}else{
 									navParent = { selected : 1, inputs : [{selected : true, name: name, hash : '#'+name, type: item.type }], name : parent.name, title : parent.title, type: item.type };
@@ -564,11 +577,11 @@
 								selected = vm.coursesList.filter( function (el){ return (el.type == 17 || el.type == 32 || el.type == 'eventos' ) && el.selected });
 								filter = vm.coursesList.filter(function(el){ return el.type == item.type });
 								if(filter.length){
-									
-									filter.map(function (el){ 
+
+									filter.map(function (el){
 												show = ( el.courses.length && (!(selected.length) || (selected.length && selected.find(function(i){ return i.type == el.parent })))) ? true : false;
-												el.show = show; 
-												el.selected = true; 
+												el.show = show;
+												el.selected = true;
 												if(show) addNavFilter(el.anchor);
 											});
 
@@ -580,7 +593,7 @@
 									filter.map( function (el, i){
 										show = (!selected.length || (selected.length && selected.find(function(i){ return i.type == el.type }))) ? true : false;
 										// inserir primeira posicao de cada lista
-										index = vm.coursesList.findIndex( function (list){ return list.parent == el.type && list.show }); 
+										index = vm.coursesList.findIndex( function (list){ return list.parent == el.type && list.show });
 										if(index < 0) index = vm.coursesList.findIndex( function (list){ return list.type == el.type });
 										if(index >= 0){
 
@@ -596,16 +609,16 @@
 												newList = { id: vm.coursesList.length+1, title : el.title+' - '+item.title, courses : courses , type: item.type, show : show , parent : el.type, selected : true, card : el.card, anchor : parent.name+'-'+item.title+'-'+el.name};
 												vm.coursesList.splice(index, 0, newList);
 											}
-											
+
 										}
 										el.show = false;
 									});
 									filter = vm.coursesList.find( function (el){ return el.type == 12 });
 									if(filter) filter.show = false;
 								}
-								
+
 							}else{
-								
+
 								selected = vm.coursesList.filter( function (el){ return  el.type == item.type });
 								selected.map( function (el){ el.show = false; el.selected = false; });
 								selected = vm.coursesList.filter( function (el){ return (el.type == 17 || el.type == 32 || el.type == "eventos" ) && el.selected });
